3ab6afae54745fc3553b9449ded751cfea452e33
elama <akalend@mail.ru>
2015-07-31 13:09:42 +0300

    add styles

--- capcha.php
+++ capcha.php
@@ -1,9 +1,27 @@
-
 <?php
     $id = rand(0,9999);
     $md5_hash = md5($id + ".12er"); 
     $security_code = substr($md5_hash, 25, 5); 
     $enc=md5($security_code);
 
-echo "id $id  $enc\n";
-?>
+    $_SESSION['count'] = $enc;
+    $secure = $_SESSION['count'];
+         // echo "--------------------------$secure<br>";
+    
+    $width = 50;
+    $height = 24; 
+    $image = ImageCreate($width, $height); 
+    $white = ImageColorAllocate($image, 255, 255, 255);
+    $black = ImageColorAllocate($image, 0, 100, 0);
+    $grey = ImageColorAllocate($image, 204, 204, 204);
+    ImageFill($image, 0, 0, $white); 
+    //Add randomly generated string in white to the image
+    ImageString($image, 10, 4, 4, $security_code, $black); 
+     // ImageRectangle($image,0,16,$width-1,$height-1,$grey); 
+     imageline($image, 0, $height/2, $width, $height/2, $grey); 
+     imageline($image, $width/2, 0, $width/2, $height, $grey); 
+    header("Content-Type: image/jpeg"); 
+    // header("Cache-Control: no-cache, must-revalidate");
+    ImageJpeg($image);
+    ImageDestroy($image);
+   
